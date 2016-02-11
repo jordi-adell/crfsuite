c2f90956c5516742e0d11befd44526bcd6528ec6
Jordi Adell <jadell@verbio.com>
2016-02-11 09:17:36 +0100

    exit on error, and stay in current dir if error, too

--- generate_and_compile.sh
+++ generate_and_compile.sh
@@ -1,18 +1,19 @@
 crfsuite_dir=`dirname $0`;
 crfsuite_dir=`readlink -m $crfsuite_dir`;
 
+set -e
+
+curr_dir=`readlink -f $PWD`
+
+trap "cd $curr_dir" EXIT
+
 ${crfsuite_dir}/autogen.sh 
-#${crfsuite_dir}/configure --prefix=${crfsuite_dir}/build --bindir=${VERBIO_TTS_BIN} --libexecdir=${VERBIO_TTS_BIN} \
-#    --libdir=${VERBIO_TTS_ROOT}/source/output/Linux/release --includedir=${VERBIO_TTS_ROOT}/source/inc/crfsuite  
 ./configure
 make -j 2
 make install
 
-#export CPATH=$CPATH:${VERBIO_TTS_ROOT}/source/inc/crfsuite 
-#export LIBRARY_PATH=$LIBRARY_PATH:${VERBIO_TTS_ROOT}/source/output/Linux/release 
-
 cd swig/python/
 ./prepare.sh
 python setup.py build_ext
-python setup.py install #  --prefix=${VERBIO_TTS_ROOT}/source/output/Linux/python/local/
-cd ../../
+python setup.py install
+
